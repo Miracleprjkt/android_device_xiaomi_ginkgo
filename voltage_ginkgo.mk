aacba84acdc6cb88e81fb12f557b8cbcc07a1a83
Miracleprjkt <Miraclev1@proton.me>
2025-06-15 13:17:37 +0700

    ginkgo: BringUp to voltage
    Signed-off-by: Miracleprjkt <Miraclev1@proton.me>

--- voltage_ginkgo.mk
+++ voltage_ginkgo.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The voltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-PRODUCT_NAME := lineage_ginkgo
+PRODUCT_NAME := voltage_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
